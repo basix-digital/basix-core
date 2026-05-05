7db744750fe7ad721acaf6a30af6910a8628f5cb
Guilherme Moreira <92859054+gmoreiraDEV@users.noreply.github.com>
2026-05-05 18:13:59 -0300

    feat(auth): register auth module in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [HealthController],
 })
 export class AppModule {}
