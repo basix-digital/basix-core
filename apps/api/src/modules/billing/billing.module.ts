2d449c4bb0ab3e4244dad556460cb3ad92af4eca
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2026-05-06 14:47:57 -0300

    feat(billing): adjustment in billing module

--- apps/api/src/modules/billing/billing.module.ts
+++ apps/api/src/modules/billing/billing.module.ts
@@ -1,1 +1,13 @@
-export {};
+import { Module } from "@nestjs/common";
+import { CommonModule } from "../common/common.module";
+import { PrismaModule } from "../prisma/prisma.module";
+import { BillingController } from "./billing.controller";
+import { BillingService } from "./billing.service";
+
+@Module({
+  imports: [CommonModule, PrismaModule],
+  controllers: [BillingController],
+  providers: [BillingService],
+  exports: [BillingService],
+})
+export class BillingModule {}
